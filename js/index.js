b250cec87c438f5a2654b77c6c82ff6f3f0c4990
jeffery_chang <e_love12134@hotmail.com>
2017-08-02 21:19:15 +0800

    1. performance improvements

--- js/index.js
+++ js/index.js
@@ -4,32 +4,34 @@ $(function(){
         el: '#indexPage',
         data:{
             slickData: pic,
-            bgData: [
-                { 'bg': 'img/bg1.png', 'mbg': 'img/m_bg1.jpg', show: true },
-                { 'bg': 'img/bg2.png', 'mbg': 'img/m_bg2.jpg', show: true },
-                { 'bg': 'img/bg3.png', 'mbg': 'img/m_bg3.jpg', show: true },
-                { 'bg': 'img/bg4.png', 'mbg': 'img/m_bg4.jpg', show: true },
-                { 'bg': '', 'mbg': 'img/m_bg5.jpg', show: false }
-            ],
             burger: false,
             albumFG: true,
+            wowFG: false,
+            singleBox: false,
+            singleBoxSrc: 'img/event/event_01.jpg',
             lightbox: true,
             lightWord: '1992-11-02 西雅圖波音公司 747-400 交機儀式暨晚宴'
         },
         beforeMount: function(){
-            $.each(pic, function(key, obj){ $.preload('img/album/' + obj.src); });
+            //$.each(pic, function(key, obj){ $.preload('img/album/' + obj.src); });
         },
         mounted: function(){
             var $this = this;
             this.menu();
-            this.scroll();
             this.initObj();
+            this.scroll();
+            this.resize();
+            this.slideBG();
 
             $(window).on('scroll', function(){
                 $this.scroll();
+            }).on('resize', function(){
+                $this.resize();
             }).on('load', function(){
-                $this.initTitle();
                 $this.lightbox = false;
+                $('#splash').delay(500).fadeOut('fast', function(){
+                    $this.initTitle();
+                });
             });
         },
         methods:{
@@ -39,17 +41,27 @@ $(function(){
             },
             menu: function(){
                 var $this = this;
-                $('.menu .left li, #overlay li').on('click', function(e){
+                $('.menu .left li, #overlay li, header .logo').on('click', function(e){
                     $this.preventall(e);
                     var dir = $(this).find('a').attr('role');
                     var padding = 0;
                     if(dir){
-                        if(dir == 'history') padding = 70;
+                        if(dir == 'fb'){
+                            return;
+                        }
+                        if(dir == 'history') padding = 100;
+                        if(dir == 'history' && $(window).width() < 450) padding = 50;
+                        if(dir == 'story' && $(window).width() < 450) padding = -100;
                         if($this.burger) $this.burger = false;
                         $this.$nextTick(function(){
-                            $('html,body').stop().animate({
+                            $('html, body').stop().animate({
                                 scrollTop: $('.' + dir).offset().top - $('header').height() / 2 - padding
                             }, 500);
+                        });
+                    }else{
+                        if($this.burger) $this.burger = false;
+                        $this.$nextTick(function(){
+                            $('html, body').stop().animate({ scrollTop: 0 }, 500);
                         });
                     }
                 });
@@ -67,6 +79,28 @@ $(function(){
                     $('header .logo img').attr('src', 'asset/svg/logo_w.svg');
                 }
             },
+            resize: function(){
+                var winWidth = $(window).width();
+                var imgName = '';
+                var fileName = 'png';
+                var position = 'center bottom';
+
+                if(winWidth <= 1080){
+                    imgName = 'm_';
+                    fileName = 'jpg';
+                    position = 'left 90%';
+                }
+                if(winWidth <= 700){
+                    imgName = 'm_';
+                    fileName = 'jpg';
+                    position = 'left -120px';
+                }
+
+                $('.inner').css({ width: $(window).innerWidth() });
+                $.each($('.cell .inner .slide-img'), function(key, obj){
+                    $(obj).attr('style', 'background: url(img/'+imgName+'bg'+(key+1)+'.'+fileName+') '+position+'; background-size: cover;');
+                });
+            },
             burgerCtrl: function(){
                 this.burger = !this.burger;
                 $('header').removeClass('scrolled');
@@ -75,12 +109,42 @@ $(function(){
                     if(!this.burger) this.scroll();
                 });
             },
+            slideBG: function(){
+                $('.inner').css({width: $(window).innerWidth()});
+                $('.cell').eq(0).addClass('is-next');
+
+                slide_show();
+                setInterval(slide_show, 5000);
+
+                function slide_show(){
+                    var final = $('.is-next').next('.cell').length; //判斷是否做後一張
+                    $('.is-next').addClass('is-active');
+                    $('.current').addClass('is-out');
+                    setTimeout(function(){
+                        if(final == 0){
+                            $('.is-next.is-active').addClass('current').removeClass('is-active is-next')
+                            $('.cell').eq(0).addClass('is-next');
+                        }else{
+                            $('.is-next.is-active').addClass('current').removeClass('is-active is-next').next('.cell').addClass('is-next');
+                        }
+                        $('.current.is-out').removeClass('current is-out');
+                    }, 1200);
+                }
+            },
             initObj: function(){
                 var $this = this;
-                
-                // 兩飛機票進場
-                new WOW().init();
-                
+
+                // WOW
+                if($(window).width() > 736){
+                    $('.timeline section .event, .timeline section .tldots, .timeline section .pic').addClass('inStage').addClass('fadeInUp');
+                    new WOW().init();
+                    new WOW({ boxClass: 'inStage', offset: 200 }).init();
+                    new WOW({ boxClass: 'timeline section', animateClass: 'long', offset: 200 }).init();
+                    this.wowFG = true;
+                }else{
+                    if(!this.wowFG) $('.timeline section').addClass('long');
+                }
+
                 // 相簿slick
                 $('.lightbox .slickBox').slick({
                     slidesToShow: 1,
@@ -105,9 +169,8 @@ $(function(){
                 }).on('beforeChange', function(event, slick, currentSlide, nextSlide){
                     $('.lightbox .slider ul li').eq(currentSlide).find('a').removeClass('active');
                     $('.lightbox .slider ul li').eq(nextSlide).find('a').addClass('active');
-
                     $.each(word, function(key, obj){
-                        if(nextSlide == obj.action) $this.lightWord = obj.words;
+                        if(nextSlide >= obj.action && nextSlide <= obj.action_end) $this.lightWord = obj.words;
                     });
                 });
 
@@ -117,19 +180,31 @@ $(function(){
                 // QA+號
                 $('.QA .content li').on('click', function(e){
                     $this.preventall(e);
-                    $('.QA .content li .plus-minus-toggle').addClass('collapsed');
-                    $('.QA .content li .answer').slideUp();
-                    $('.QA .content li').removeClass('active');
-                    $(this).find('.plus-minus-toggle').removeClass('collapsed');
-                    $(this).addClass('active');
-                    $(this).find('.answer').slideDown();
+                    if($(this).hasClass('active')){
+                        $(this).find('.plus-minus-toggle').addClass('collapsed');
+                        $(this).removeClass('active');
+                        $(this).find('.answer').slideUp();
+                    }else{
+                        $('.QA .content li .plus-minus-toggle').addClass('collapsed');
+                        $('.QA .content li .answer').slideUp();
+                        $('.QA .content li').removeClass('active');
+                        $(this).find('.plus-minus-toggle').removeClass('collapsed');
+                        $(this).addClass('active');
+                        $(this).find('.answer').slideDown();
+                    }
                 });
 
                 // 歷史回顧
                 $('.slidebox').slick({
+                    dots: true,
+                    appendDots: $('.story .slider'),
                     infinite: false,
+                    adaptiveHeight: true,
                     prevArrow: $('.story_arrowbox .left'),
-                    nextArrow: $('.story_arrowbox .right')
+                    nextArrow: $('.story_arrowbox .right'),
+                    customPaging : function(slider, i) {
+                        return '<a style="cursor: pointer;"></a>';
+                    },
                 }).on('beforeChange', function(event, slick, currentSlide, nextSlide){
                     $('.indicator').css('margin-left', nextSlide * 25 + 'px');
                 });
@@ -139,11 +214,12 @@ $(function(){
                 var right = $('.tt2');
                 var line = $('.tt4');
                 var plane = $('.tt5');
+                var words = $('.index .sub:not(.m_sub)');
                 var delaytime = 0;
                 var sec = 1;
 
                 TweenMax.to(right, .8, {width: "144px", delay: .75 + delaytime});
-                TweenMax.staggerTo(left, .75, {y: -50, opacity: 1, delay: delaytime, ease: Power1.easeOut}, .33);
+                TweenMax.staggerTo([left, words], .75, {y: -50, opacity: 1, delay: delaytime, ease: Power1.easeOut}, .33);
 
                 var tl = new TimelineMax({delay: .5 + delaytime});
                 var bezTween = new TweenMax(plane, sec, {
@@ -164,10 +240,19 @@ $(function(){
                 var newWord = word[key].words;
                 var newKey = word[key].action;
                 this.lightWord = newWord;
-                $('.lightbox .slickBox').on('reInit').slick('slickGoTo', newKey, true);
+
+                $('.lightbox .slickBox').slick('slickGoTo', newKey, true);
                 $('.lightbox .slider ul').slick('slickGoTo', newKey, true);
 
                 this.lightbox = true;
+                this.$nextTick(function(){
+                    $('.lightbox .slickBox').slick('setPosition');
+                });
+            },
+            openSingle: function(e){
+                this.preventall(e);
+                this.singleBoxSrc = e.target.getAttribute('src');
+                this.singleBox = true;
             }
         }
     });
